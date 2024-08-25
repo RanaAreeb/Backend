74feeecfef2f5d8d925ade67cf72c5c65161555e
Rana Muhammad Areeb <115886881+RanaAreeb@users.noreply.github.com>
2024-08-25 23:13:43 +0500

    added

---
+++ src/AuthPage.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import exampleImage from './assets/img.jpg'; // Ensure the correct file extension and path
+
+const AuthPage = (props) => {
+  // State to manage the username input value
+  const [username, setUsername] = useState('');
+
+  // Function to handle form submission
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Post the username to the API
+    axios.post('http://localhost:3001/authenticate', { username })
+      .then(r => props.onAuth({ ...r.data, secret: username }))
+      .catch(e => console.log('error', e));
+  }
+
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <img src={exampleImage} alt="AnonGram Logo" />
+        <div className="form-title">Welcome to AnonGram👋</div>
+
+        <div className="form-subtitle">Set a username to get started</div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input
+            className="auth-input"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <button className="auth-button" type="submit">
+            Enter
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
